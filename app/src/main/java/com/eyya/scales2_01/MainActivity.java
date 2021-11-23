9c3201de3b0da828ff6b3c0443c5f7aa2d509e67
AliHachem <ali@eyya.com>
2021-11-23 17:43:01 +0000

    Scans for devices. Prints to commandline.

--- app/src/main/java/com/eyya/scales2_01/MainActivity.java
+++ app/src/main/java/com/eyya/scales2_01/MainActivity.java
@@ -1,14 +1,120 @@
 package com.eyya.scales2_01;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
+    private final int ENABLE_BLUETOOTH_REQUEST_CODE = 1;
+    private final int intVal = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+        /*
+         * Check if device supports BLE
+         */
+        if (bAdapter == null) {
+            Toast.makeText(this, "Device Does not support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        /*
+          Check if Bluetooth is on
+          Request to turn on
+         */
+        assert bAdapter != null;
+        if (!bAdapter.isEnabled()) {
+            Intent bTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            launchBTRequestActivity.launch(bTIntent);
+        }
+        initLocationPermission();
+        getPairedDevices(bAdapter);
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+        bAdapter.startDiscovery();
     }
+
+    private void getPairedDevices(BluetoothAdapter bAdapter) {
+        // Get paired devices.
+        Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+            }
+        }
+    }
+
+    private void makeDiscoverable() {
+        /*
+         * Set device discoverable
+         * device becomes discoverable for 300 seconds
+         */
+        Intent dIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        dIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(dIntent);
+    }
+
+    /*
+     * Bluetooth request activity launch
+     */
+    ActivityResultLauncher<Intent> launchBTRequestActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // your operation....
+                }
+            });
+
+    private void initLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                System.out.println("***********" + deviceName);
+
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
 }
